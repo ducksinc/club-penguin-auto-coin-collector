3addb3b32ad971064118bdd9131ac08fb738c2e0
Chris <44344493+ChrisDaDerp@users.noreply.github.com>
2022-12-28 02:47:59 -0800

    Added speed based on distance, but needs improvement

--- cp_auto_clicker/newcp_auto_clicker.py
+++ cp_auto_clicker/newcp_auto_clicker.py
@@ -10,26 +10,35 @@ Authors:
 import pyautogui as pg 
 import time
 import random
+import os
+import math
 
-DELAY_MOVES = 15
-# Timer for mouse movement.
-D = 1
-# Number of moves to be made by auto clicker. 8 cycles is enough to collect the Puffle treasure at the right time with default configuration.
+DELAY_MOVEMENT = 15
+'''Timer to delay next movement. Default config optimized for mining the most coins.'''
+DELAY_TIME = .2
+'''Timer for mouse movement.'''
 SIZE = 8
-# Images folder location.
+'''Number of moves to be made by auto clicker. 8 cycles is enough to collect the Puffle treasure at the right time with default configuration.'''
 FOLDER_PATH = "./img/"
-GROUND_FOLDER_PATH = FOLDER_PATH + "ground/"
-# Ground image list.
+'''Images folder location.'''
 GROUND_COLORS = [[103, 80, 76], [91, 70, 67], [87, 67, 64], [95, 73, 70]]
-# Icon matcher settings.
+'''Ground RGB values list.'''
 GRAYSCALE = True
+'''Grayscale is true by default.'''
 CONFIDENCE = .9
+'''Confidence default.'''
+SPEED = 260
+'''Penguin movement speed, in pixels/sec (measured at 2560x1440).'''
 
-# AutoClicker class. Handles all moves.
 class AutoClicker():
+    '''
+    AutoClicker class. Handles all moves.
+    '''
     __size = 0
     __current = 0
     __debug = None
+    __puffle = None
+    __speed = 0
     __screen_size = None
     __screen_left = 0
     __screen_top = 0
@@ -43,7 +52,9 @@ class AutoClicker():
         self.__current = 0
         self.__debug = False
         self.__screen_size = pg.size()
-        # Coordinates for area that the program will search to see if it is open to be clicked on.
+        self.__puffle = None
+        # Coordinate math for area that the program will search to see if it is open to be clicked on. Also for determining penguin speed in pixels/second.
+        self.__speed = SPEED * ((self.__screen_size[0] / 2560) ** 2)
         # 0 is for width and 1 is for height.
         self.__screen_left = round(self.__screen_size[0]*.2617)
         self.__screen_top = round(self.__screen_size[1]*.5382)
@@ -53,70 +64,92 @@ class AutoClicker():
         self.__width_range = round(self.__screen_size[0]*.7988)
         self.__height_range =  round(self.__screen_size[1]*.8924)
 
-    # Prints screen size. For debugging.
+
     def __print_screen_size(self):
+        '''Prints screen size. For debugging.'''
         print("Screen size: height=" + str(self.__screen_size[1]) + ", width=" + str(self.__screen_size[0]))
         print("Borders: left=" + str(self.__screen_left) + ", top=" + str(self.__screen_top) + ", width=" + str(self.__screen_width) + ", height=" + str(self.__screen_height))
         pg.screenshot("screen_borders.png", region=(self.__screen_left, self.__screen_top, self.__screen_width, self.__screen_height))
 
-    # Gets current position. For debugging.
     def __get_pos(self):
+        '''Gets current position. For debugging.'''
         return pg.position()
 
-    # Clicks penguin then mines.
     def __click_mine(self):
+        '''Clicks penguin then mines.'''
         penguin_icon_location = pg.locateCenterOnScreen(FOLDER_PATH + "penguin_icon.png", grayscale=GRAYSCALE, confidence=CONFIDENCE)
         pg.click(penguin_icon_location)
-        time.sleep(D)
+        time.sleep(DELAY_TIME)
         dance_icon_location = pg.locateCenterOnScreen(FOLDER_PATH + "dance_icon.png", grayscale=GRAYSCALE, confidence=CONFIDENCE)
         pg.click(dance_icon_location)
-        time.sleep(D)
+        time.sleep(DELAY_TIME)
 
-    # Moves cursor to coordinates and then clicks. Set on a delay to maximize profits from mining.
     def __move_click(self, current):
+        '''Moves cursor to coordinates and then clicks. Set on a delay to maximize profits from mining.'''
         found = False
         while True:
             x = random.randint(self.__screen_left, self.__width_range)
             y = random.randint(self.__screen_top, self.__height_range)
+            new_position = [x, y]
             for ground_color in GROUND_COLORS:
-                if pg.pixelMatchesColor(x, y, (ground_color[0], ground_color[1], ground_color[2])):
+                if pg.pixelMatchesColor(new_position[0], new_position[1], (ground_color[0], ground_color[1], ground_color[2])):
                     found = True
                 if found == True:
+                    current_position = pg.position()
+                    movement_time = self.__calc_distance(current_position, new_position)
                     break
             if found == True:
                 break
             
-        pg.moveTo(x, y, .1)
         pg.click(x, y)
-        time.sleep(D*1.3)
+        time.sleep(movement_time)
         self.__click_mine()
-        time.sleep(DELAY_MOVES)
+        time.sleep(DELAY_MOVEMENT)
 
-    # Clicks Puffle icon, then money bag.
     def __run_puf(self):
-        puffle_icon_location = pg.locateCenterOnScreen(FOLDER_PATH + "puffle_icon.png", grayscale=GRAYSCALE, confidence=CONFIDENCE)
-        pg.click(puffle_icon_location)
-        time.sleep(D)
-        money_icon_location = pg.locateCenterOnScreen(FOLDER_PATH + "money_icon.png", grayscale=GRAYSCALE, confidence=CONFIDENCE)
-        if self.__debug == True:
-            if money_icon_location == None:
-                print("Puffle money icon either not found or not yet ready to be collected.")
-        pg.click(money_icon_location)
-        time.sleep(D)
-
-    # Recursively calls auto_click. Starts with getting Puffle money bag, then runs until current is equal to size, then resets current to 0 and loops.
-    def auto_click(self, current):
-        while True:
-            if current == 0:
+        '''Clicks Puffle icon, then money bag.'''
+        # Figures out which Puffle you have and sets that as the default for as long as the program runs.
+        if self.__puffle == None:
+            for image in os.listdir(FOLDER_PATH + "puffles/"):
+                if pg.locateOnScreen(FOLDER_PATH + "puffles/" + image, grayscale=self.__debug, confidence=CONFIDENCE):
+                    self.__puffle = FOLDER_PATH + "puffles/" + image
+                    if self.__debug == True:
+                        print("Puffle found: " + image)
+                    break
+            if self.__puffle == None:
+                self.__puffle = False
                 if self.__debug == True:
+                    print("No Puffle found.")
+        # Finds Puffle on the screen and then clicks it.
+        try:
+            puffle_icon_location = pg.locateCenterOnScreen(self.__puffle, grayscale=GRAYSCALE, confidence=CONFIDENCE)
+            pg.click(puffle_icon_location)
+            time.sleep(DELAY_TIME)
+            # Finds money bag on the screen and clicks it, if it is ready.
+            money_icon_location = pg.locateCenterOnScreen(FOLDER_PATH + "money_icon.png", grayscale=GRAYSCALE, confidence=CONFIDENCE)
+            if self.__debug == True:
+                if money_icon_location == None:
+                    print("Puffle money icon either not found or not yet ready to be collected.")
+            pg.click(money_icon_location)
+            time.sleep(DELAY_TIME)
+        except:
+            if self.__debug == True:
+                print("No Puffle found. Continuing.")
+
+    def auto_click(self, current):
+        '''Recursively calls auto_click. Starts with getting Puffle money bag, then runs until current is equal to size, then resets current to 0 and loops.'''
+        while True:
+            if current == self.__size:
+                current = 0
+            if current == 0:
+                if self.__debug == True and self.__puffle != False:
                     print("Running Puffle...")
                     if self.__get_pos() != None:
                         print("Current location: " + str(self.__get_pos()))
                     else:
                         print("Unable to find cursor location.")
-                self.__run_puf()
-            if current == SIZE:
-                current = 0
+                if self.__puffle != False:
+                    self.__run_puf()
             if self.__debug == True:
                 print("Running iteration " + str(current))
                 if self.__get_pos() != None:
@@ -126,19 +159,29 @@ class AutoClicker():
             self.__move_click(current)
             self.auto_click(current+1)
 
-    # Returns current.
+    def __calc_distance(self, current_position, new_position):
+        '''Calculates pixels traveled.'''
+        movement_time = math.hypot(abs(current_position[1] - new_position[1]), abs(current_position[0] - new_position[0]))
+        if self.__debug == True:
+            print("Speed=" + str(self.__speed))
+            print("x1=" + str(current_position[1]) + " x2=" + str(new_position[1]) + " y1=" + str(current_position[0]) + " y2=" + str(new_position[0]))
+            print("Pixels to travel=" + str(movement_time))
+            print("Movement time=" + str(movement_time / self.__speed))
+        return (movement_time  / self.__speed)
+
     def get_current(self):
+        '''Returns current.'''
         return self.__current
 
-    # Sets debug mode (default off).
     def set_debug(self, mode):
+        '''Sets debug mode (default off).'''
         print("Debug mode on.")
         self.__debug = mode
         if mode == True:
             self.__print_screen_size()
 
-# Creates and runs auto clicker.
 def main():
+    '''Creates and runs auto clicker.'''
     start = input("Run auto clicker? (y/n): ")
     if start.lower() == "y" or "d":
         auto_clicker = AutoClicker()
